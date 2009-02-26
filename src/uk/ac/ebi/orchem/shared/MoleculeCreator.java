f38e6bbbfbf69af5ad55ce409c06e55569c1eb18
Mark Rynbeek <markrynbeek@gmail.com>
2009-02-26 15:40:08 +0000

    moved out of Utils class

---
+++ src/uk/ac/ebi/orchem/shared/MoleculeCreator.java
@@ -0,0 +1,87 @@
+/*  
+ *  $Author$
+ *  $Date$
+ *  $Revision$
+ *
+ *  Copyright (C) 2008-2009  OrChem project
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *  All we ask is that proper credit is given for our work, which includes
+ *  - but is not limited to - adding the above copyright notice to the beginning
+ *  of your source code files, and to any copyright notice that you may distribute
+ *  with programs based on this work.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *
+ */
+
+package uk.ac.ebi.orchem.shared;
+
+import java.io.StringReader;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.openscience.cdk.aromaticity.CDKHueckelAromaticityDetector;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.io.MDLV2000Reader;
+import org.openscience.cdk.nonotify.NNMolecule;
+import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
+
+/**
+ * Class for creating molecules
+ */
+public class MoleculeCreator {
+
+    /**
+     * Creates a molecule using an MDL string and an MDL reader
+     * @param mdlReader
+     * @param mdlString
+     * @return
+     * @throws CDKException
+     */
+    public static NNMolecule getNNMolecule(MDLV2000Reader mdlReader, String mdlString) throws CDKException {
+        NNMolecule molecule = null;
+        mdlReader.setReader(new StringReader(mdlString));
+        molecule = new NNMolecule();
+
+        try {
+            molecule = (NNMolecule)mdlReader.read(molecule);
+        } catch (NullPointerException e) {
+            /* Fix for NullPointer due to occurence of D or T (Deuterium or Tritium) with massNumber null
+             * as happens now and then in Starlite (Chembl)
+             * Regex fix to replace D or T with a H (Hydrogen) symbol as replacement.
+             */
+            Pattern p = Pattern.compile("[D|T](\\s+\\d){6}");
+            Matcher m = p.matcher(mdlString);
+            StringBuilder sb = new StringBuilder(mdlString);
+            while (m.find()) {
+                sb.replace(m.start(),m.start()+1,"H");
+            }
+            //retry, hopefully NullPointer fixed...
+            mdlString=sb.toString();
+            mdlReader.setReader(new StringReader(mdlString));
+            molecule = new NNMolecule();
+            molecule = (NNMolecule)mdlReader.read(molecule);
+        }
+
+
+        NNMolecule nnMolecule = new NNMolecule(AtomContainerManipulator.removeHydrogens(molecule));
+        if (nnMolecule == null || nnMolecule.getAtomCount() == 0)
+            throw new RuntimeException("Error parsing molfile is null or mol atom count is zero. ");
+
+        AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(nnMolecule);
+        CDKHueckelAromaticityDetector.detectAromaticity(nnMolecule);
+        return nnMolecule;
+    }
+    
+    
+
+}

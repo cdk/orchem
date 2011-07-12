ef83ebbe77910982fb5c076cce7f730a7cab0fce
Mark Rynbeek <markrynbeek@gmail.com>
2011-07-12 10:50:27 +0000

    oops

--- test/step2_loadjava.bash
+++ test/step2_loadjava.bash
@@ -12,17 +12,17 @@ echo
 
 
 
-#dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/vecmath.jar
-#dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/jama-1.0.2.jar
-#dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/commons-collections-3.2.1.jar
-#dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/jgrapht-0.6.0.jar
-#dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/log4j-1.2.15.jar
-#dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/nestedVm.jar
-#dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/inchi103.jar
-#dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/cdk.jar
-#dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/ambit2.smarts.jar
-#dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/jcp.jar
-#dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST /tmp/orchem.jar
+dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/vecmath.jar
+dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/jama-1.0.2.jar
+dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/commons-collections-3.2.1.jar
+dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/jgrapht-0.6.0.jar
+dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/log4j-1.2.15.jar
+dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/nestedVm.jar
+dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/inchi103.jar
+dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/cdk.jar
+dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/ambit2.smarts.jar
+dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST $JARDIR/jcp.jar
+dropjava  -nowarn -user $DBUSER/$DBPASS\@$DBINST /tmp/orchem.jar
 
 
 
